f1c7a327e0650f0bdb602471dc35e99e14ebb0e2
VERJUS Benjamin <etu43859@henallux.be>
2019-10-02 14:49:05 +0200

    Ajout du bruteForce

---
+++ atelier/brute.py
@@ -0,0 +1,29 @@
+
+passwd = ""
+alphabet ="ABCDEFGHIJKLMNOPQRSTUVWXTYZ"
+
+for letter in alphabet:
+    char1 = letter
+
+    for letter in alphabet:
+        char2 = letter
+
+        for letter in alphabet:
+            char3 = letter
+
+            for letter in alphabet:
+                char4 = letter
+
+                for letter in alphabet:
+                    char5 = letter
+
+                    for letter in alphabet:
+                        char6 = letter
+
+                        for letter in alphabet:
+                            char7 = letter
+
+                            for letter in alphabet:
+                                char8 = letter  
+                                passwd = char1 + char2 + char3 + char4 + char5 + char6 + char7 + char8
+                                print(passwd + "\n")
