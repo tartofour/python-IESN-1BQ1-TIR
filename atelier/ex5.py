e1cdce9879f2a149545f04e93c7ac2d1a00b810d
tartof <benjamin.verjus@protonmail.com>
2019-10-06 18:31:47 +0200

    Correction ex5.py

--- atelier/ex5.py
+++ atelier/ex5.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
-import math
+from math import pi
 
 rayon = float(input("Entrez un rayon en mètre : "))
-circonference = 2 * math.pi * rayon
-aire = math.pi * rayon**2
+circonference = 2 * pi * rayon
+aire = pi * rayon**2
 
 print("Circonférence : {:.2f}m \nAire : {:.5f}m²".format(circonference, aire))
 
