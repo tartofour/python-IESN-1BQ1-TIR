6af4105aa3f24b42a5161cda02110dc84ab23572
tartof <benjamin.verjus@protonmail.com>
2019-10-23 22:19:20 +0200

    Ajout de la 2e série d'exercices et suppression des fichiers/dossiers inutiles.

--- atelier/ex7.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-
-nb_user = int(input("Entrez un nombre : "))
-print(nb_user %2 == 0)
-
--- atelier/ex8.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-nb_user = float(input("Entrez un nombre entier positif : "))
-partie_entiere = nb_user // 1
-partie_decimale = nb_user % 1 
-
-print("{} = {} + {}".format(nb_user, partie_entiere, partie_decimale))
