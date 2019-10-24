4f3aa8346b04c26ede250b4666ab9cde18578294
tartof <benjamin.verjus@protonmail.com>
2019-10-24 19:50:11 +0200

    Correction de certains exercices

--- exercices/serie1/ex13.py
+++ exercices/serie1/ex13.py
@@ -8,8 +8,7 @@ nb_batiments_valide = nb_batiments != 0 and nb_batiments <= 15
 ressources_insuffisantes = nb_batiments * ressources_necessaires > ressources_disponibles 
 peut_construire =  nb_batiments != 0 and nb_batiments <= 15 and nb_batiments * ressources_necessaires <= ressources_disponibles
 
-print("")
-print("Le nombre de bâtiment est-il valide ? : {}".format(nb_batiments_valide))
+print("\nLe nombre de bâtiment est-il valide ? : {}".format(nb_batiments_valide))
 print("Les ressources sont elles insuffisantes ? : {}".format(ressources_insuffisantes))
 print("Pouvez-vous contruire ? : {}".format(peut_construire))
 
