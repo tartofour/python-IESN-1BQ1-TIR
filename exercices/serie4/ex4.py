60aac644e88ac996baa23079e687ded2c2daee2d
tartof <benjamin.verjus@protonmail.com>
2019-12-05 12:02:48 +0100

    Modification d'ex4 et ex5

--- exercices/serie4/ex4.py
+++ exercices/serie4/ex4.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 
 def remove_negatives(lst: list) -> None:
-    lst2 = []
-    for elt in lst:
-        if elt >= 0 :
-            lst2.append(elt)
-    print(lst2)
+    i = len(lst) - 1
+    while i >= 0:
+        if lst[i] < 0 :
+            del lst[i] 
+        i -= 1
+    print(lst)
 
 liste = [-23, -2, 28, 23, -23, -12, 1]
 remove_negatives(liste)
--- exercices/serie4/ex5.py
+++ exercices/serie4/ex5.py
@@ -2,21 +2,17 @@
 
 def split_even_odd(lst: list) -> tuple:
     i = 0
-    lst_even = []
-    lst_odd = []
+    lst_even_index = []
+    lst_odd_index = []
     length = len(lst)
     while i < length:  
         if i % 2 == 0:
-            lst_even.append(lst[i])
+            lst_even_index.append(lst[i])
         else:
-            lst_odd.append(lst[i])
+            lst_odd_index.append(lst[i])
         i += 1
-    return (lst_even,lst_odd)
+    return (lst_even_index,lst_odd_index)
 
 liste = [22, 21, 35, 53, 22, 24]
 print(liste)
 print(split_even_odd(liste))
-
-
-
-
