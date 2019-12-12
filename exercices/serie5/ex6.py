d3720da2cf7a1c26c917da226b1fb391f68b0de6
tartof <benjamin.verjus@protonmail.com>
2019-12-12 14:31:29 +0100

    Ajout de ex6.py

--- exercices/serie5/ex6.py
+++ exercices/serie5/ex6.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python
 
-### PAS FINI !!! ###
-
 def common_pairs(d1: dict, d2: dict) -> list:
     lst_common_value = []
     for elmt in d1:
-        if elmt in d2:
-        lst_common_value.append((elmt))
+        if elmt in d2 and d1[elmt] in d2[elmt]: 
+                tpl_common_value = elmt, d1[elmt]
+                lst_common_value.append(tpl_common_value)
     return lst_common_value
 
 
-d1 = {1 : "Hi", 2 : "Bonjour", 3 : "Prout"}
-d2 = {1 : "Hi", 2 : "Aurevoir", 3 : "Prit"}
+d1 = {1 : "Hi", 2 : "Bonjour", 3 : "Prout", 4 : "Coucou"}
+d2 = {1 : "Hi", 2 : "Aurevoir", 3 : "Prit", 4 : "Coucou"}
 print(common_pairs(d1, d2))
